67259465793b87ec4b6b9a0e688f18de64fd0fd9
paul.ciobanu <cpaul06@gmail.com>
2020-11-21 12:37:23 +0200

    Implement client side

--- Client/Client.cs
+++ Client/Client.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Grpc.Core;
+using Shared;
 
 namespace Client
 {
@@ -6,7 +8,24 @@ namespace Client
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = args[0];
+            var port = int.Parse(args[1]);
+            var x = long.Parse(args[2]);
+            var op = args[3];
+            var y = long.Parse(args[4]);
+            var channel = new Channel(
+                host,
+                port,
+                ChannelCredentials.Insecure);
+
+            var client = new CalcService.CalcServiceClient(channel);
+            var reply = client.Calculate(new CalculateRequest
+            {
+                X = x,
+                Y = y,
+                Op = op
+            });
+            Console.WriteLine($"The calculated result is: {reply.Result}");
         }
     }
 }
