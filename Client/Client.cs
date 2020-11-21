331130034dda674c04aed88a15ba35faed0016ce
paul.ciobanu <cpaul06@gmail.com>
2020-11-21 12:00:40 +0200

    Project structure

---
+++ Client/Client.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Client
+{
+    internal class Client
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Server
+{
+    internal class Server
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
